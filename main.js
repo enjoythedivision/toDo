8d0ab57fb34ffb931f6eb7024d5327eb0c447b12
Maria <147405522+enjoythedivision@users.noreply.github.com>
2026-04-04 19:39:16 +0300

    Refreshing for loops

--- main.js
+++ main.js
@@ -13,27 +13,25 @@
 //document.querySelector("p#weather").style.color = "red"; //only the first element with this id will be changed
 //document.querySelector("p#weather").style.fontSize = "20px"; //only the first element with this id will be changed
 
-
 // EVENTS
 document.querySelector("#open-nav-menu").addEventListener("click", function () {
   document.querySelector("header nav .wrapper").classList.add("nav-open");
 });
 
-document.querySelector("#close-nav-menu").addEventListener("click", function () {
-  document.querySelector("header nav .wrapper").classList.remove("nav-open");
-});
-
+document
+  .querySelector("#close-nav-menu")
+  .addEventListener("click", function () {
+    document.querySelector("header nav .wrapper").classList.remove("nav-open");
+  });
 
 // VARIABLES
 let customer = "Maria"; // let -> block scope variable -> available only within the block it is defined
 //var customer = "Maria"; // var -> global variable -> available anywhere
 const pi = 3.14; // const -> constant variable -> cannot be reassigned
 
-
 // CONSOLE
 console.log("Hello " + customer); // Hello Maria
 console.log(`Hello ${customer}!`);
-
 
 // GREETING SECTION
 const greetingText = "Good morning!!";
@@ -54,7 +52,7 @@ document.querySelector("#weather").innerHTML = weatherText;
 let array1 = [2, 4, 6, 8];
 let student = {
   name: "John",
-  age: 20
+  age: 20,
 }; //this is an object with two properties: name and age
 student.name = "Maria"; // change the name property of the student object
 student.age = 25; // change the age property of the student object
@@ -62,12 +60,11 @@ student.grade = 10; // add a new property to the student object
 let student2 = {
   name: "Anna",
   age: 22,
-  grade: 9
+  grade: 9,
 }; //this is another object with three properties: name, age and grade
 
 let students = [student, student2]; // this is an array of objects!
 students.length; // 2
-
 
 //BOOLEANS
 num1 = 8;
@@ -79,9 +76,6 @@ num1 === num2; // === -> strict equality operator -> checks if the values && the
 //undefined: we never defined a value for this var
 //null: we defined a value but it is empty (SEE MORE ON THIS.)
 
-
-
-
 ////FUNCTIONS
 //no semicolons after functions, only on statements, eg celsiustofahr();
 function updateWeather() {
@@ -90,36 +84,80 @@ function updateWeather() {
 }
 
 function convertToFahrenheit(celsius) {
-  let fahr = ((celsius * 9/5) + 32).toFixed();
+  let fahr = ((celsius * 9) / 5 + 32).toFixed();
   return Number(fahr);
 }
 
 function convertToCelsius(fahr) {
-  let celsius = ((fahr - 32) * 5/9).toFixed();
+  let celsius = (((fahr - 32) * 5) / 9).toFixed();
   return Number(celsius);
 }
 
-document.querySelector(".weather-group").addEventListener("click", function(e){ //pls see more on e
-  console.log(e.target.id);
-  if (e.target.id == "celsius" && tempUnit !== "C") {
-    temperature = convertToCelsius(temperature);
-    tempUnit = "C";
-    updateWeather();
-  }
-  else if (e.target.id == "fahr" && tempUnit !== "F") {
-    temperature = convertToFahrenheit(temperature);
-    tempUnit = "F";
-    updateWeather();
-  }
-});
+document
+  .querySelector(".weather-group")
+  .addEventListener("click", function (e) {
+    //pls see more on e
+    console.log(e.target.id);
+    if (e.target.id == "celsius" && tempUnit !== "C") {
+      temperature = convertToCelsius(temperature);
+      tempUnit = "C";
+      updateWeather();
+    } else if (e.target.id == "fahr" && tempUnit !== "F") {
+      temperature = convertToFahrenheit(temperature);
+      tempUnit = "F";
+      updateWeather();
+    }
+  });
 
 //DATE AND TIME
 //new Date().getHours();
 let localTime = new Date();
-document.querySelector("span[data-time=hours]").textContent = localTime.getHours();
-document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes();
-document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds();
 
+//TIMEOUT
+setTimeout(function () {
+  console.log("inside the timeout");
+}, 3000); //delaying. setInterval for every x seconds
+
+setInterval(function () {
+  let localTime = new Date();
+  document.querySelector("span[data-time=hours]").textContent =
+    localTime.getHours().toString().padStart(2, "0");
+  document.querySelector("span[data-time=minutes]").textContent =
+    localTime.getMinutes().toString().padStart(2, "0");
+  document.querySelector("span[data-time=seconds]").textContent =
+    localTime.getSeconds().toString().padStart(2, "0");
+}, 1000); //check later
+
+//PADSTART
+myString = "5";
+myString.padStart(2, "0"); //(how many digits i want, what i want to add)
+
+//LOOPS
+//for loop
+for (let i = 0; i < 5; i++) {
+  //code...
+}
+
+//while loop
+let i = 0;
+while (i < 5) {
+  //code...
+  i++;
+}
+
+//do while loop
+let j = 0;
+do {
+  //code...
+  j++;
+}
+while (j < 5);
+
+//for arrays
+let arr = [1, 2, 5, 7, 21];
+for (let a = 0; a < arr.length; a++) {
+  console.log(arr[a]);
+}
 
 
 
